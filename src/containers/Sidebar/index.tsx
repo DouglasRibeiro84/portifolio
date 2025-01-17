5ca8d1af0cb5675d3a9604fa375f38f4ed97f575
doouglas <douglasribeiro084@gmail.com>
2025-01-17 07:43:39 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/DouglasRibeiro84.png" />
-    <Titulo fontSize={20}>Douglas</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Douglas</Titulo>
+      <Paragrafo fontSize={16} tipo="secundario">
+        DouglasRibeiro84
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Dev FrontEnd
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
